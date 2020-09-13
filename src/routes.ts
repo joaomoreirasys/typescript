3d5c038bc6ce5095f2859730983bede637475195
João Moreira <joaomoreira.a.s@hotmail.com>
2020-09-13 10:06:17 -0300

    tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
-    return response.json({ message: "hello world" });
+    const user = createUser({
+        email: 'joaomoreira.a.s@gmail.com',
+        password: '123456',
+        techs: [
+            'Node.js',
+            'ReactJS',
+            'React Native',
+            { title: 'javascript', experience: 100 }
+        ],
+    });
+    return response.json({ user });
 }
